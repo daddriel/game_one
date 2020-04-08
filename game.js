7440667afb7501f7952e5fe9250215f4e9fb99c7
David Adriazola <d.adriazolamuriel@gmail.com>
2020-04-08 17:13:06 -0400

    GAME01-01:branch SWITCH.
    -first commit, version using switch

--- game.js
+++ game.js
@@ -1,29 +1,48 @@
 var player;
 var moveMachine = ["piedra", "papel", "tijera"];
 var machine;
-
+var playerMachine;
 function game(player){
-       
     machine = moveMachine[Math.round(Math.random()*2)];
+    playerMachine = player + machine;
+
     if(player != "piedra" && player != "papel" && player != "tijera"){
         console.log("Jugada no valida 😡")
-    }else if(player === machine){
-        console.log("Empate 🙃")
-    }else if(player == "piedra" && machine == "tijera"){
-        console.log("Ganaste 🥳");
-        print(player, machine);
-    }else if(player == "papel" && machine == "piedra"){
-        console.log("Ganaste 🥳");
-        print(player, machine);
-    }else if(player == "tijera" && machine == "papel"){
-        console.log("Ganaste 🥳");
-        print(player, machine);
-    }else
-        console.log("Perdiste 💩");
         print(player, machine);
 
+    }else{
+        switch(playerMachine){
+            case "piedrapiedra":
+                console.log("Empate 😅");
+                print(player,machine);
+                break;
+            case "papelpapel":
+                console.log("Empate 😅");
+                print(player,machine);
+                break;
+            case "tijeratijera":
+                console.log("Empate 😅");
+                print(player,machine);
+                break;     
+            case "piedratijera":
+                console.log("Ganaste 🥳");
+                print(player,machine);
+                break;
+            case "papelpiedra":
+                console.log("Ganaste 🥳");
+                print(player,machine);
+                break;
+            case "tijerapapel":
+                console.log("Ganaste 🥳");
+                print(player,machine);
+                break;
+            default:
+                console.log("Perdiste 💩");
+                print(player,machine);
+                break;
+        }
+    }    
 }
-
 function print(player,machine){
 if(player==="papel"){
     console.log("Player 🖐");
